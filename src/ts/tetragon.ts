eb9560fd122f09c8ffa1011a6b85f43afdb1f0b9
Sebastian Langer <langer@bestit-online.de>
2019-07-05 20:03:50 +0200

    add tedra support for gyroscope

--- src/ts/tetragon.ts
+++ src/ts/tetragon.ts
@@ -3,12 +3,21 @@ let isLocked = true;
 
 const $tetragon = document.querySelector('.tetragon') as HTMLDivElement;
 
+const clamp = (val: number, min: number, max: number) => Math.max(Math.min(val, max), min);
+
 window.addEventListener('mousemove', throttle(100, e => {
     if(isLocked){
         return;
     }
     const xFac = e.clientX / window.innerWidth * 2 - 1;
     const yFac = e.clientY / window.innerHeight * 2 - 1;
+    $tetragon.style.transform = `rotateY(${xFac * 15}deg) rotateX(${yFac * -15}deg)`;
+}));
+
+window.addEventListener('deviceorientation', throttle(100, e => {
+    const xFac = clamp(e.gamma, -45, 45) / 45;
+    const yFac = clamp(e.beta, -45, 45) / 45;
+    console.log(xFac, yFac)
     $tetragon.style.transform = `rotateY(${xFac * 15}deg) rotateX(${yFac * -15}deg)`;
 }));
 
