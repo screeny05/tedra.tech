be59a9047223289b6765edfabb3fbb7a382beb3c
Sebastian Langer <langer@bestit-online.de>
2020-07-29 17:40:34 +0200

    #2 tetragon - do not listen on touch disguised as mousemove

--- src/ts/tetragon.ts
+++ src/ts/tetragon.ts
@@ -3,6 +3,7 @@ import { throttle } from 'throttle-debounce';
 window.addEventListener('load', () => {
     let isPlaying = false;
     let hasPlayedOnce = false;
+    let isTouchStarted = false;
     const $tetragon = document.querySelector('.tetragon') as HTMLDivElement;
     const $soundcloudIframe = document.querySelector('.js-soundcloud-iframe') as HTMLIFrameElement|null;
 
@@ -32,7 +33,14 @@ window.addEventListener('load', () => {
         hasPlayedOnce = true;
     };
 
+    window.addEventListener('touchstart', () => isTouchStarted = true);
+
     window.addEventListener('mousemove', throttle(100, (e: MouseEvent) => {
+        if(isTouchStarted){
+            isTouchStarted = false;
+            return;
+        }
+
         const xFac = e.clientX / window.innerWidth * 2 - 1;
         const yFac = e.clientY / window.innerHeight * 2 - 1;
         $tetragon.style.transform = `rotateY(${xFac * 15}deg) rotateX(${yFac * -15}deg)`;
