915313b8653ea98b4f236967cd6f211bfac272c9
Sebastian Langer <sl@scn.cx>
2023-01-02 22:42:40 +0100

    use newer node version for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact({
-    compat: true
-  })],
-  output: "server",
-  adapter: netlify()
+  integrations: [
+    preact({
+      compat: true,
+    }),
+  ],
+  output: 'static',
 });
