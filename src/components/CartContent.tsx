4b07d3b9202b757d94cae743a48c803f6c6f81d6
Sebastian Langer <sl@scn.cx>
2023-02-14 00:41:20 +0100

    Re-implement previously existing components

--- src/components/CartContent.tsx
+++ src/components/CartContent.tsx
@@ -135,32 +135,55 @@ export function CartContent() {
   };
 
   if (orderData) {
+    const shipping = orderData.purchase_units?.[0]?.shipping;
+
     return (
-      <div className="cart-content cart-content--empty">
-        <div>
-          <p>Purchase successful.</p>
-          <p>Thank you for supporting TEDRA!</p>
-          <p>You will receive an order-confirmation via mail, as soon as your order has been reviewed.</p>
-        </div>
+      <div class="section__text--center">
+        <h2>Purchase successful.</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="150px" height="150px">
+          <path fill="currentColor" d="M 25 2 C 12.317 2 2 12.317 2 25 C 2 37.683 12.317 48 25 48 C 37.683 48 48 37.683 48 25 C 48 20.44 46.660281 16.189328 44.363281 12.611328 L 42.994141 14.228516 C 44.889141 17.382516 46 21.06 46 25 C 46 36.579 36.579 46 25 46 C 13.421 46 4 36.579 4 25 C 4 13.421 13.421 4 25 4 C 30.443 4 35.393906 6.0997656 39.128906 9.5097656 L 40.4375 7.9648438 C 36.3525 4.2598437 30.935 2 25 2 z M 43.236328 7.7539062 L 23.914062 30.554688 L 15.78125 22.96875 L 14.417969 24.431641 L 24.083984 33.447266 L 44.763672 9.046875 L 43.236328 7.7539062 z" />
+        </svg>
+        <p>Thank you for supporting TEDRA!</p>
+        <p>You will receive an order-confirmation via mail, as soon as your order has been reviewed.</p>
+        {shipping ? (
+          <>
+            <p>Your order data:</p>
+            <code class="cart-content__order-data">
+              <pre>
+                {shipping.name.full_name}
+                {'\n'}
+                {shipping.address.address_line_1}
+                {'\n'}
+                {shipping.address.postal_code} {shipping.address.admin_area_1}
+                {'\n\n'}
+                Order ID: {orderData.id}
+              </pre>
+            </code>
+          </>
+        ) : null}
       </div>
     );
   }
 
   if (basket.items.length === 0) {
     return (
-      <div className="cart-content cart-content--empty">
-        <div>
-          <p>No items in the basket, yet.</p>
-          <p>
-            <a href="/#shop">Have a look at our items.</a>
-          </p>
+      <>
+        <div class="section__title">Basket.</div>
+        <div className="cart-content cart-content--empty">
+          <div>
+            <p>No items in the basket, yet.</p>
+            <p>
+              <a href="/#shop">Have a look at our items.</a>
+            </p>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 
   return (
     <PayPalScriptProvider options={{ 'client-id': import.meta.env.PUBLIC_PAYPAL_CLIENT_ID, currency: 'EUR' }}>
+      <div class="section__title">Basket.</div>
       <div className="cart-content">
         <div className="cart-content__items">
           {basket.items.map((item) => (
