06167f95e564a2ee9fcf77e453d5e495ceb15dab
Sebastian Langer <sebastian.langer@laioutr.com>
2025-07-11 23:11:17 +0200

    Add Kessel Festival 2025 YouTube video

--- src/data/youtube-videos.ts
+++ src/data/youtube-videos.ts
@@ -1,6 +1,7 @@
 import type { YoutubeVideo } from '../models/youtube-video';
 
 export const youtubeVideos: YoutubeVideo[] = [
+  { id: '19i1slZ-pZw', title: 'TEDRA | Live at Kessel Festival 2025' },
   { id: 'VipuxVEqLjw', title: 'TEDRA | Live at Blu Sky Festival' },
   { id: 'DCRJshHrbK8', title: 'TEDRA | Live at UtzUtz Series Closing' },
   { id: 'BfNw2YlySdg', title: 'TEDRA | Live at Electronic Village 2022' },
